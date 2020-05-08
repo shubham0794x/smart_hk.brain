3536f3be819ebbefa89451c3ed4db4b0510ad81e
Shubham Bhadani <shubham.bhadani07@gmail.com>
2020-05-08 23:36:11 +0530

    Render

--- src/App.js
+++ src/App.js
@@ -74,6 +74,38 @@ onInputChange = (event) => {
   this.setState({input: event.target.value});
 }
 
+render() {
+  const { isSignedIn, imageUrl, route, box } = this.state;
+  return (
+    <div className="App">
+       <Particles className='particles'
+        params={particlesOptions}
+      />
+      <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+      { route === 'home'
+        ? <div>
+            <Logo />
+            <Rank
+              name={this.state.user.name}
+              entries={this.state.user.entries}
+            />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            <FaceRecognition box={box} imageUrl={imageUrl} />
+          </div>
+        : (
+           route === 'signin'
+           ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+           : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+          )
+      }
+    </div>
+  );
+}
+}
+
 
 
 
