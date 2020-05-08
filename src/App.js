a53bfff3aac6c59bf74d7055c49c7a467ca3c14a
Shubham Bhadani <shubham.bhadani07@gmail.com>
2020-05-08 23:28:51 +0530

    FaceBox

--- src/App.js
+++ src/App.js
@@ -44,7 +44,26 @@ const particlesOptions = {
   }
 }
 
+calculateFaceLocation = (data) => {
+  const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+  const image = document.getElementById('inputimage');
+  const width = Number(image.width);
+  const height = Number(image.height);
+  return {
+    leftCol: clarifaiFace.left_col * width,
+    topRow: clarifaiFace.top_row * height,
+    rightCol: width - (clarifaiFace.right_col * width),
+    bottomRow: height - (clarifaiFace.bottom_row * height)
+  }
+}
 
+displayFaceBox = (box) => {
+  this.setState({box: box});
+}
+
+onInputChange = (event) => {
+  this.setState({input: event.target.value});
+}
 
 
 
