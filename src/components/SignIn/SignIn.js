dffed93a23ea2719ad6005c8cdaf14139188669f
Shubham Bhadani <shubham.bhadani07@gmail.com>
2020-05-08 23:58:46 +0530

    Update SignIn.js

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/signin', {
+    fetch('', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -72,7 +72,7 @@ class Signin extends React.Component {
                 value="Sign in"
               />
             </div>
-            <div className="lh-copy mt3">
+            <div className="">
               <p  onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
             </div>
           </div>
